d7cc0591b2d04ea8d53e6a3cdc1d98eb713ffbd1
SachiniRasanga <sachisach983@gmail.com>
2023-10-22 21:01:02 +0530

    updatescreens_update

---
+++ app/src/main/java/com/example/rp_project_frontend/RecommendedDoctorDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.rp_project_frontend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecommendedDoctorDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended_doctor_details);
+    }
+}
