059dea90bc3f9e29978bf021223a442ce8113d25
SachiniRasanga <sachisach983@gmail.com>
2023-10-23 17:06:36 +0530

    doctorpage

---
+++ app/src/main/java/com/example/rp_project_frontend/RecommendedDoctorActivity.java
@@ -0,0 +1,17 @@
+package com.example.rp_project_frontend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecommendedDoctorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommended_doctor);
+
+    }
+
+
+}
