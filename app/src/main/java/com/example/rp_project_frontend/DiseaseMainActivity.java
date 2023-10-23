86895eb0489c067fb8f6317196d4a1359897e421
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-10-23 13:29:40 +0530

    Disease Identify screen link

--- app/src/main/java/com/example/rp_project_frontend/DiseaseMainActivity.java
+++ app/src/main/java/com/example/rp_project_frontend/DiseaseMainActivity.java
@@ -1,14 +1,38 @@
 package com.example.rp_project_frontend;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class DiseaseMainActivity extends AppCompatActivity {
+
+    CardView voiceChatBtn, findDoctorBtn, knowledgeBaseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_disease_main);
+
+        voiceChatBtn = findViewById(R.id.disease_identify_voice_chat_btn);
+        findDoctorBtn = findViewById(R.id.disease_identify_find_doctor_btn);
+        knowledgeBaseBtn = findViewById(R.id.disease_identify_knowledge_base_btn);
+
+        voiceChatBtn.setOnClickListener(view -> {
+            Intent voiceChatIntent = new Intent(DiseaseMainActivity.this, VoiceChatsActivity.class);
+            startActivity(voiceChatIntent);
+        });
+
+        findDoctorBtn.setOnClickListener(view -> {
+            Intent findDoctorIntent = new Intent(DiseaseMainActivity.this, RecommendedDoctorDetailsActivity.class);
+            startActivity(findDoctorIntent);
+        });
+
+        knowledgeBaseBtn.setOnClickListener(view -> {
+            Intent knowledgeBaseIntent = new Intent(DiseaseMainActivity.this, KnowledgebaseActivity.class);
+            startActivity(knowledgeBaseIntent);
+        });
     }
 }
