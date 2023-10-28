fc7254aab12556ab461f0a9f520f2df5e2b643e4
SachiniRasanga <sachisach983@gmail.com>
2023-10-28 09:18:06 +0530

    rotateimage

--- app/src/main/java/com/example/rp_project_frontend/UploadImageActivity.java
+++ app/src/main/java/com/example/rp_project_frontend/UploadImageActivity.java
@@ -3,6 +3,8 @@ package com.example.rp_project_frontend;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -12,16 +14,33 @@ public class UploadImageActivity extends AppCompatActivity {
 
     ImageView uploadImageView;
 
+     Button rotateButton;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_uploadimage);
 
         uploadImageView = findViewById(R.id.presImageView);
+        rotateButton = findViewById(R.id.rotatebutton);
 
         Intent intent = getIntent();
         Uri image = Uri.parse(intent.getStringExtra("image"));
 
         uploadImageView.setImageURI(image);
+
+        rotateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rotateImage();
+            }
+        });
+    }
+
+    private void rotateImage() {
+
+        float currentRotation = uploadImageView.getRotation();
+        currentRotation += 90;
+        uploadImageView.setRotation(currentRotation);
     }
 }
