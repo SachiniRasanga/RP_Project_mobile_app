c69480ec27c22977c386a975dc4e5b3cb459b8d7
Tharuka Gayashan <tharuka.gayashan.5@gmail.com>
2023-10-28 10:19:55 +0530

    API connect to text identify

--- app/src/main/java/com/example/rp_project_frontend/ClearTextPrescriptionActivity.java
+++ app/src/main/java/com/example/rp_project_frontend/ClearTextPrescriptionActivity.java
@@ -1,14 +1,28 @@
 package com.example.rp_project_frontend;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClearTextPrescriptionActivity extends AppCompatActivity {
+
+    CardView clearTextDisplaySpace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clear_text_prescription);
+
+        clearTextDisplaySpace = findViewById(R.id.clear_text_display_space);
+
+        Intent uploadImageIntent = getIntent();
+        ArrayList<String> predicted_class = uploadImageIntent.getStringArrayListExtra("predicted_class");
+        clearTextDisplaySpace.setActivated(true);
+        clearTextDisplaySpace.setTooltipText(predicted_class.get(0));
     }
 }
--- app/src/main/java/com/example/rp_project_frontend/Retrofit/MLRetrofitClient.java
+++ app/src/main/java/com/example/rp_project_frontend/Retrofit/MLRetrofitClient.java
@@ -1,21 +1,31 @@
 package com.example.rp_project_frontend.Retrofit;
 
-import com.example.rp_project_frontend.API.AuthEndpoints;
 import com.example.rp_project_frontend.API.DiseaseIdentifyEndpoints;
 import com.example.rp_project_frontend.API.TextIdentifyEndpoints;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MLRetrofitClient {
-    private static String BASE_URL = "http://192.168.1.102:8000";
+    private static String BASE_URL = "http://192.168.1.100:8000";
     private static MLRetrofitClient retrofitClient;
     private static Retrofit retrofit;
 
     private MLRetrofitClient() {
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectionPool(new ConnectionPool(5, 10, TimeUnit.MINUTES)) // Adjust pool settings as needed
+                .callTimeout(100,TimeUnit.SECONDS)
+                .build();
+
         retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
     }
 
